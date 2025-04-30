5dae852b1fe28b32a6439666bccab141f2eeb4c0
ajmastra <ajers03@gmail.com>
2025-04-29 18:58:05 -0700

    Added Save and Skip overlays to the swiping functionality - Created class for dynamic screen boundaries depedning on phone screen size

---
+++ sip-n-swipe/Core/Cards/Views/SwipeActionIndicatorView.swift
@@ -0,0 +1,18 @@
+//
+//  SwipeActionIndicatorView.swift
+//  sip-n-swipe
+//
+//  Created by AJ Mastrangelo on 4/29/25.
+//
+
+import SwiftUI
+
+struct SwipeActionIndicatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwipeActionIndicatorView()
+}

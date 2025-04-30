1f5f8fcd43a061d860809d70bc9b45891ca05fb9
ajmastra <ajers03@gmail.com>
2025-04-29 20:47:42 -0700

    Added CardStackView to the maintabview

---
+++ sip-n-swipe/Core/SavedRecipes/Views/SavedRecipesView.swift
@@ -0,0 +1,18 @@
+//
+//  SavedRecipesView.swift
+//  sip-n-swipe
+//
+//  Created by AJ Mastrangelo on 4/29/25.
+//
+
+import SwiftUI
+
+struct SavedRecipesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SavedRecipesView()
+}

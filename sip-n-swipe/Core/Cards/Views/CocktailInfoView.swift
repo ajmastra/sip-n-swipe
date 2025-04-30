beca258e1aeeb592386afa49ab26df88a8fc00da
ajmastra <ajers03@gmail.com>
2025-04-29 18:24:15 -0700

    Basic swiping funtionality and card view setup

---
+++ sip-n-swipe/Core/Cards/Views/CocktailInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  CocktailInfoView.swift
+//  sip-n-swipe
+//
+//  Created by AJ Mastrangelo on 4/29/25.
+//
+
+import SwiftUI
+
+struct CocktailInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CocktailInfoView()
+}
---
+++ sip-n-swipe/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  sip-n-swipe
+//
+//  Created by AJ Mastrangelo on 4/29/25.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}

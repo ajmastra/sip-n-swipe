e4b01fcf7acfadf177138eed859ab92ea598e099
ajmastra <ajers03@gmail.com>
2025-04-30 01:26:29 -0700

    Saving recipes funtionality, fixed #6, #3, #2, #1

---
+++ sip-n-swipe/Core/SavedRecipes/Views/CocktailDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  CocktailDetailView.swift
+//  sip-n-swipe
+//
+//  Created by AJ Mastrangelo on 4/30/25.
+//
+
+import SwiftUI
+
+struct CocktailDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CocktailDetailView()
+}
